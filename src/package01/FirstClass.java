1b06cf870233c3f1c694b119fbce83e8d776ac7d
Aziz <azizekeathn@gmail.com>
2020-06-21 22:33:29 +0300

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
 		System.out.println("Ikinci commit from locak repository");
+		
+		System.out.println("Yeni branch olsuturdum ve adi feature");
 	}
 
 }
